2609d6939668aed9f2eddc437da0ab4ba7aa7636
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2016-09-19 19:07:56 +1200

    Configuration for git.

---
+++ git/git_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#title          :git_config.sh
+#description    :Script for configuring git.
+#author         :Jarrod N. Bakker
+#date           :19/09/2016
+#usage          :bash git_config.sh
+#========================================================================
+git config --global user.name “Jarrod N. Bakker”
+git config --global user.email jarrodbakker@hotmail.com
+git config --global core.editor vim
+git config --global push.default simple
+
