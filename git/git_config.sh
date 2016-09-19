021dc3208f58d104c445f6acf02c3846fa9c0bb8
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2016-09-19 22:19:54 +1200

    Quotation mark fix.

--- git/git_config.sh
+++ git/git_config.sh
@@ -5,7 +5,7 @@
 #date           :19/09/2016
 #usage          :bash git_config.sh
 #========================================================================
-git config --global user.name “Jarrod N. Bakker”
+git config --global user.name "Jarrod N. Bakker"
 git config --global user.email jarrodbakker@hotmail.com
 git config --global core.editor vim
 git config --global push.default simple
