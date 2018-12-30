978fd281e9eb8ae40184418107f4e3632123b542
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2018-12-30 05:17:31 +0000

    Updated with Python pip and dev.

--- bash/bash_install_common_pkgs.sh
+++ bash/bash_install_common_pkgs.sh
@@ -3,11 +3,11 @@
 #description    :Script for install useful packages that I use on most
 #	         Linux systems.
 #author         :Jarrod N. Bakker
-#date           :23/09/2016
+#date           :30/12/2017
 #usage          :bash bash_install_common_pkgs.sh
 #========================================================================
 sudo apt-get update;
 sudo apt-get upgrade -y;
-sudo apt-get install vim tmux python-dev python3-pip openssh-server -y;
+sudo apt-get install vim tmux python python-dev python-pip python3 python3-dev python3-pip openssh-server -y;
 sudo apt autoremove -y;
 
