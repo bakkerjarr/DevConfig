8779e540c3254994002186d1e7c0ee5a950772b2
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2020-11-04 21:45:08 +1300

    Continued development: bin directory setup and looking into .vimrc

--- shell_setup/setup-shell.sh
+++ shell_setup/setup-shell.sh
@@ -26,17 +26,36 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 0
 fi
 
-if [ -d "~/bin" ]; then
+# See if $HOME/bin already exists and create it if not
+if [ -d "$HOME/bin" ]; then
     echo "Directory already exists (skipping creation): \"$HOME/bin\""
 else
     echo "Directory does not exist (creating): "$HOME/bin""
-    #TODO: mkdir "$HOME/bin"
+    #mkdir "$HOME/bin"
     echo "Setting permissions on \"$HOME/bin\" to octal 700..."
-    #TODO: chmod 700 "$HOME/bin"
+    #chmod 700 "$HOME/bin"
 fi
 
-# Append ~/bin $HOME/bin to PATH
-if [ ! -z $(grep 'PATH=$PATH' "$HOME/.bash_profile") ]; then echo "FOUND"; fi
-#TODO grep the above grep command for ~/bin or $HOME/bin
-#TODO back-up the .bash_profile file before editing it: cp -p "$HOME/.bash_profile" "$HOME/.bash_profile.`date+%F`"
+# Append $HOME/bin to PATH
+if [ ! -z $(grep 'PATH=$PATH' "$HOME/.bash_profile" | grep '$HOME/bin\|~/bin') ]; then
+    echo "\"$HOME/.bash_profile\" already has \"\$HOME/bin\" or \"~/bin\" set in the PATH variable"
+else
+    echo "Adding \"\$HOME/bin\" to the PATH variable in \"$HOME/.bash_profile\"..."
+    #cp -p "$HOME/.bash_profile" "$HOME/.bash_profile.`date +%F`"
+    #sed -i '/^PATH=\$PATH/ s/$/\:\$HOME\/bin/' "$HOME/.bash_profile"
+fi
 
+# Copy your user's bin scripts to $HOME/bin
+echo "Copying scripts to \"$HOME/bin\"..."
+#cp -i "./bin_scripts/*" "$HOME/bin"
+#chmod -R 700 "$HOME/bin"
+
+# Copy over the .vimrc
+if [ -f "$HOME/.vimrc" ]; then
+    echo "WARNING: \"$HOME/.vimrc\" already exists and will be backed up"
+    #cp -p "$HOME/.vimrc" "$HOME/.vimrc.`date +%F`"
+fi
+#TODO if system is RHEL-based copy over vimrc to ~/ || if system is Debian-based copy over vimrc_debian to ~/
+
+# Configure your user's git client if it is installed
+#TODO Is git installed? -> which git
