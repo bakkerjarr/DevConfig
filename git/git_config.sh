c8a0f52c9443e70481f7cef80b4372d0dff2e08d
Jarrod Bakker <bakkerjarr@users.noreply.github.com>
2024-05-21 14:45:49 +1200

    Set default branch name to 'main'

--- git/git_config.sh
+++ git/git_config.sh
@@ -9,4 +9,5 @@ git config --global user.name "Jarrod N. Bakker"
 git config --global user.email jarrodbakker@hotmail.com
 git config --global core.editor vim
 git config --global push.default simple
+git config --global init.defaultBranch main
 ssh-keyscan github.com >> ~/.ssh/known_hosts
